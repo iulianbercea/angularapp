48cd260432014a3cc2bd38a41339803597dd72d1
iulianbercea <iulianbercea@yahoo.com>
2015-12-07 21:01:35 +0000

    07/12/2101

--- js/app.js
+++ js/app.js
@@ -5,3 +5,30 @@ app.controller("headerController", function($scope){
     $scope.logoUrl = "../img/logo.jpg";
     
 });
+
+app.controller("adsController", function($scope){
+    
+    $scope.advert = [
+        {
+            url: "../img/ad1.jpg"
+        },
+        {
+            url: "../img.ad2.jpg"
+        },
+        {
+            url: "../img/ad3.jpg"
+        },
+        {
+            url: "../img.ad4.jpg"
+        },
+        {
+            url: "../img.ad5.jpg"
+        },
+        {
+            url: "../img.ad6.jpg"
+        }
+        ];
+        
+    
+    
+});
